6bc3669c2f7a2268e1b2f36ceef92ee3339b4631
JeongUk Park <jeongph.dev@gmail.com>
2025-11-05 23:59:54 +0900

    feat: 커밋 누락 추가(.vitepress)

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,5 @@
+export default {
+  title: 'prac-vitepress',               // 탭 타이틀
+  description: 'VitePress 연습용 프로젝트',
+  base: '/prac-vitepress/',              // GitHub Pages에서 필수(레포 이름과 동일)
+}
